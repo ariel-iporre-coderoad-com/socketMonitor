832c31562b52fe201a7a85c64fda5fa103e1abe6
ariel <ariel.iporre@mojix.com>
2016-07-26 08:06:32 -0400

    corePerformanceReader script added

---
+++ corePerfomanceReader.py
@@ -0,0 +1,32 @@
+"""
+Core Performance Reader
+
+reads log an extract perfomance informations.
+@author Ariel Iporre
+
+"""
+import numpy as np
+import subprocess
+import time
+
+def getNativeFiltersTime():
+    fileName = "/home/ariel/Documents/CorePerformance/MCBwithOut.log"
+    word = "LPT.native: "
+    marks = ["sourceFilter=", "pointInZone=", "doorEvent=", "shiftZone=", "identifiers=", "total="]
+    marksPositions = [8, 9, 10, 11, 12, 13]
+    with open(fileName) as f:
+        for line in f:
+            if word in line:
+                a = []
+                # print line
+                b = line[:-1].split(" ")
+                cnt = 0
+                for mark in marks:
+                    position = marksPositions[cnt]
+                    cnt += 1
+                    c = b[position]
+                    offset = len(mark)
+                    a.append(c[offset:])
+                print a[0] + " " + a[1] + " " + a[2] + " " + a[3] + " " + a[4] + " " + a[5]
+
+getNativeFiltersTime()
